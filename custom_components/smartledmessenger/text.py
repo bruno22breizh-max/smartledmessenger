775bbbf8ea75f2fec68356d4f75fa60b85386fc3
bruno22breizh-max <bruno22breizh@gmail.com>
2025-12-21 22:24:20 +0100

    Update text.py

--- custom_components/smartledmessenger/text.py
+++ custom_components/smartledmessenger/text.py
@@ -9,22 +9,30 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities([SmartLedText(coordinator, entry)])
 
 class SmartLedText(CoordinatorEntity, TextEntity):
-    _attr_name = 'Smart Led Messenger'
-    _attr_icon = 'mdi:led-on'
+    _attr_name = "Smart Led Messenger"
+    _attr_icon = "mdi:led-on"
 
     def __init__(self, coordinator, entry):
         super().__init__(coordinator)
         self._entry = entry
-        self._attr_native_value = ''
+        self._attr_native_value = ""
 
     async def async_set_value(self, value: str) -> None:
+        # 🔑 mémorisation du message
+        self.coordinator.last_message = value
+
         data = self._entry.data
         message = quote(value)
-        url = f'http://{data["ip"]}/?message={message}&intensity={data["intensity"]}&speed={data["speed"]}'
+        url = (
+            f"http://{data['ip']}/"
+            f"?message={message}"
+            f"&intensity={data['intensity']}"
+            f"&speed={data['speed']}"
+        )
+
         async with aiohttp.ClientSession() as session:
             await session.get(url)
+
         self._attr_native_value = value
         self.async_write_ha_state()
-        coordinator.last_message = value
-
 
