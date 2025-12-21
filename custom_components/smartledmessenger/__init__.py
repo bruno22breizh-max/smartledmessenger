409b2bbc6a8751c3c6072dc4b90f9d0c9df54018
bruno22breizh-max <bruno22breizh@gmail.com>
2025-12-21 22:00:09 +0100

    Update __init__.py

--- custom_components/smartledmessenger/__init__.py
+++ custom_components/smartledmessenger/__init__.py
@@ -1,13 +1,40 @@
-from .const import DOMAIN
-from .coordinator import SmartLedCoordinator
+import aiohttp
+from urllib.parse import quote
+
+from .const import DOMAIN, CONF_INTENSITY, CONF_SPEED
 
 async def async_setup_entry(hass, entry):
-    coordinator = SmartLedCoordinator(hass, entry.data['ip'])
-    await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(entry, ['text', 'number', 'button'])
+    hass.data[DOMAIN][entry.entry_id] = entry
+
+    async def handle_send(call):
+        message = call.data["message"]
+        intensity = call.data.get("intensity", entry.data[CONF_INTENSITY])
+        speed = call.data.get("speed", entry.data[CONF_SPEED])
+
+        msg = quote(message)
+        url = (
+            f"http://{entry.data['ip']}/"
+            f"?message={msg}&intensity={intensity}&speed={speed}"
+        )
+
+        async with aiohttp.ClientSession() as session:
+            await session.get(url)
+
+    hass.services.async_register(
+        DOMAIN,
+        "send",
+        handle_send,
+    )
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["text", "number", "button"]
+    )
     return True
 
+
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_unload_platforms(entry, ['text', 'number', 'button'])
+    hass.services.async_remove(DOMAIN, "send")
+    return await hass.config_entries.async_unload_platforms(
+        entry, ["text", "number", "button"]
+    )
