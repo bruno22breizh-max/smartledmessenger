6843dbf5507edcf17f0832a52ff047f61d0840f2
bruno22breizh-max <bruno22breizh@gmail.com>
2025-12-25 20:59:32 +0100

    Update __init__.py

--- custom_components/smartledmessenger/__init__.py
+++ custom_components/smartledmessenger/__init__.py
@@ -1,2 +1,19 @@
-"""Smart LED Messenger integration."""
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
+from .const import DOMAIN
+
+PLATFORMS = ["text", "number", "switch", "button", "select"]
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
