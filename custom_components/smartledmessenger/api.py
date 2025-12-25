95cfec0bcf701c808c54c8ea9977899e4d6d99a7
bruno22breizh-max <bruno22breizh@gmail.com>
2025-12-25 21:00:06 +0100

    Change send method to synchronous requests
    Refactor SmartLedAPI to use requests instead of aiohttp for sending messages.

--- custom_components/smartledmessenger/api.py
+++ custom_components/smartledmessenger/api.py
@@ -1,21 +1,23 @@
-import aiohttp
+import requests
 
 class SmartLedAPI:
     def __init__(self, host):
         self.host = host
 
-    async def send(self, params):
-        url = f"http://{self.host}/"
-        async with aiohttp.ClientSession() as session:
-            await session.get(url, params=params)
-
-    async def show_message(self, message, intensity=10, speed=40, static=0):
-        await self.send({
-            "message": message,
-            "intensity": intensity,
-            "speed": speed,
-            "static": static
-        })
-
-    async def show_clock(self):
-        await self.send({"message": ""})
+    def send(
+        self,
+        message="",
+        intensity=10,
+        speed=40,
+        static=0,
+        local=0,
+    ):
+        url = (
+            f"http://{self.host}/"
+            f"?message={message}"
+            f"&intensity={intensity}"
+            f"&speed={speed}"
+            f"&static={static}"
+            f"&local={local}"
+        )
+        requests.get(url, timeout=5)
